4c3fa455651760bd8dcf8345eb7af6bc993507f7
chirivilorenzo <chirivilorenzo@gmail.com>
2024-04-30 12:18:30 +0200

    db nuovo

--- DB/limonta.sql
+++ DB/limonta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Apr 30, 2024 alle 11:14
+-- Creato il: Apr 30, 2024 alle 12:18
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -20,6 +20,44 @@ SET time_zone = "+00:00";
 --
 -- Database: `limonta`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `aperturaticket`
+--
+
+CREATE TABLE `aperturaticket` (
+  `ID` int(11) NOT NULL,
+  `codiceCliente` char(6) NOT NULL,
+  `stato` enum('aperto','chiuso','sospeso','annullato') NOT NULL,
+  `area` enum('Area PC e reti','AS400','Java','Contabilità','Formatori','Derma','Terzisti','Commerciali') NOT NULL,
+  `breveDescrizione` varchar(64) NOT NULL,
+  `descrizione` text NOT NULL,
+  `dataApertura` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dump dei dati per la tabella `aperturaticket`
+--
+
+INSERT INTO `aperturaticket` (`ID`, `codiceCliente`, `stato`, `area`, `breveDescrizione`, `descrizione`, `dataApertura`) VALUES
+(1, 'gg1234', 'aperto', 'Area PC e reti', '', 'no', '2024-04-19 13:48:53');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `chiusuraticket`
+--
+
+CREATE TABLE `chiusuraticket` (
+  `ID` int(11) NOT NULL,
+  `IDticket` int(11) NOT NULL,
+  `IDdipendente` int(11) NOT NULL,
+  `dataFine` datetime NOT NULL,
+  `risoluzioneProblema` varchar(128) NOT NULL,
+  `altro` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -71,46 +109,33 @@ INSERT INTO `dipendente` (`ID`, `nome`, `cognome`, `username`, `password`, `emai
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `dipendente_ricket`
+-- Struttura della tabella `dipendente_ticket`
 --
 
-CREATE TABLE `dipendente_ricket` (
-  `ID` int(11) NOT NULL,
+CREATE TABLE `dipendente_ticket` (
   `idDipendente` int(11) NOT NULL,
   `idTicket` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- --------------------------------------------------------
-
---
--- Struttura della tabella `ticket`
---
-
-CREATE TABLE `ticket` (
-  `ID` int(11) NOT NULL,
-  `codiceCliente` char(6) NOT NULL,
-  `stato` enum('aperto','chiuso','sospeso','annullato') NOT NULL,
-  `area` enum('Area PC e reti','AS400','Java','Contabilità','Formatori','Derma','Terzisti','Commerciali') NOT NULL,
-  `breveDescrizione` varchar(64) NOT NULL,
-  `descrizione` text NOT NULL,
-  `dataApertura` datetime NOT NULL,
-  `categoria` varchar(32) NOT NULL,
-  `userDipendente` varchar(16) NOT NULL,
-  `soluzioneProblema` varchar(64) DEFAULT NULL,
-  `dataChiusura` datetime DEFAULT NULL,
-  `idTicketRif` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dump dei dati per la tabella `ticket`
---
-
-INSERT INTO `ticket` (`ID`, `codiceCliente`, `stato`, `area`, `breveDescrizione`, `descrizione`, `dataApertura`, `categoria`, `userDipendente`, `soluzioneProblema`, `dataChiusura`, `idTicketRif`) VALUES
-(1, 'gg1234', 'aperto', 'Area PC e reti', '', 'no', '2024-04-19 13:48:53', 'niente', 'gianluca', NULL, '2024-04-19 13:48:53', NULL);
-
 --
 -- Indici per le tabelle scaricate
 --
+
+--
+-- Indici per le tabelle `aperturaticket`
+--
+ALTER TABLE `aperturaticket`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `emailCliente` (`codiceCliente`),
+  ADD UNIQUE KEY `codiceCliente` (`codiceCliente`);
+
+--
+-- Indici per le tabelle `chiusuraticket`
+--
+ALTER TABLE `chiusuraticket`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `IDticket` (`IDticket`,`IDdipendente`),
+  ADD KEY `IDdipendente` (`IDdipendente`);
 
 --
 -- Indici per le tabelle `cliente`
@@ -130,26 +155,29 @@ ALTER TABLE `dipendente`
   ADD UNIQUE KEY `username` (`username`,`email`,`numTelefono`);
 
 --
--- Indici per le tabelle `dipendente_ricket`
+-- Indici per le tabelle `dipendente_ticket`
 --
-ALTER TABLE `dipendente_ricket`
-  ADD PRIMARY KEY (`ID`),
+ALTER TABLE `dipendente_ticket`
+  ADD PRIMARY KEY (`idDipendente`,`idTicket`),
+  ADD UNIQUE KEY `idDipendente_2` (`idDipendente`,`idTicket`),
   ADD KEY `idDipendente` (`idDipendente`,`idTicket`),
   ADD KEY `idTicket` (`idTicket`);
 
 --
--- Indici per le tabelle `ticket`
---
-ALTER TABLE `ticket`
-  ADD PRIMARY KEY (`ID`),
-  ADD UNIQUE KEY `emailCliente` (`codiceCliente`),
-  ADD UNIQUE KEY `codiceCliente` (`codiceCliente`),
-  ADD KEY `idTicketRif` (`idTicketRif`),
-  ADD KEY `userDipendenteChiuso` (`userDipendente`);
-
---
 -- AUTO_INCREMENT per le tabelle scaricate
 --
+
+--
+-- AUTO_INCREMENT per la tabella `aperturaticket`
+--
+ALTER TABLE `aperturaticket`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT per la tabella `chiusuraticket`
+--
+ALTER TABLE `chiusuraticket`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `cliente`
@@ -164,35 +192,28 @@ ALTER TABLE `dipendente`
   MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT per la tabella `dipendente_ricket`
---
-ALTER TABLE `dipendente_ricket`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `ticket`
---
-ALTER TABLE `ticket`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- Limiti per le tabelle scaricate
 --
 
 --
--- Limiti per la tabella `dipendente_ricket`
+-- Limiti per la tabella `aperturaticket`
 --
-ALTER TABLE `dipendente_ricket`
-  ADD CONSTRAINT `dipendente_ricket_ibfk_1` FOREIGN KEY (`idDipendente`) REFERENCES `dipendente` (`ID`),
-  ADD CONSTRAINT `dipendente_ricket_ibfk_2` FOREIGN KEY (`idTicket`) REFERENCES `ticket` (`ID`);
+ALTER TABLE `aperturaticket`
+  ADD CONSTRAINT `aperturaticket_ibfk_4` FOREIGN KEY (`codiceCliente`) REFERENCES `cliente` (`codice`);
 
 --
--- Limiti per la tabella `ticket`
+-- Limiti per la tabella `chiusuraticket`
 --
-ALTER TABLE `ticket`
-  ADD CONSTRAINT `ticket_ibfk_2` FOREIGN KEY (`idTicketRif`) REFERENCES `ticket` (`ID`),
-  ADD CONSTRAINT `ticket_ibfk_3` FOREIGN KEY (`userDipendente`) REFERENCES `dipendente` (`username`),
-  ADD CONSTRAINT `ticket_ibfk_4` FOREIGN KEY (`codiceCliente`) REFERENCES `cliente` (`codice`);
+ALTER TABLE `chiusuraticket`
+  ADD CONSTRAINT `chiusuraticket_ibfk_1` FOREIGN KEY (`IDticket`) REFERENCES `aperturaticket` (`ID`),
+  ADD CONSTRAINT `chiusuraticket_ibfk_2` FOREIGN KEY (`IDdipendente`) REFERENCES `dipendente` (`ID`);
+
+--
+-- Limiti per la tabella `dipendente_ticket`
+--
+ALTER TABLE `dipendente_ticket`
+  ADD CONSTRAINT `dipendente_ticket_ibfk_1` FOREIGN KEY (`idDipendente`) REFERENCES `dipendente` (`ID`),
+  ADD CONSTRAINT `dipendente_ticket_ibfk_2` FOREIGN KEY (`idTicket`) REFERENCES `aperturaticket` (`ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
